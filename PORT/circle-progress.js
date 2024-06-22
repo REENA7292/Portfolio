64f8f97eaa0e76cc166940148c17e0301af8054f
REENA SRI S <147152632+REENA7292@users.noreply.github.com>
2024-06-22 20:13:19 +0530

    Add files via upload

---
+++ PORT/circle-progress.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import 'js-circle-progress'
+
+const circleprogress = () => {
+  const cp = document.createElement('circle-progress')
+
+}
+
+export default circle-progress
